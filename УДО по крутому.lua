7f9630f1d7817a4ad8299d8205ebe84812482247
arthur29k <60294105+arthur29k@users.noreply.github.com>
2025-09-14 19:44:11 +0300

    version1.1

--- УДО по крутому.lua
+++ УДО по крутому.lua
@@ -1,6 +1,6 @@
 script_author("arthur29k")
 script_name("УДО по крутому")
-script_version("1.0.2")
+script_version("1.1")
 
 local inicfg = require "inicfg"
 require "kurmanin.kys"
@@ -45,8 +45,8 @@ function main()
     sampRegisterChatCommand("parole_help", function ()
         sampShowDialog(12101, "УДО ПО КРУТОМУ | Помощь", "{ffffff}«Пасибочки, что используете скрипт, мне очень приятно :3», сказал arthur29k.\n\nВот для вас команды:\n/fuckoff и /comeback - выключить/перезапустить скрипт\n"..
         "/parole - меню настроек скрипта\n"..
-        "/getjail ID - Получить сумму по удо для заключённого\n"..
-        "/unpunish [ID] [SUMM] - Освободить заключённого по удо\n"..
+        "/getjail ID - Получить сумму по УДО для заключённого\n"..
+        "/unpunish [ID] [SUMM] - Освободить заключённого по УДО\n"..
         "/jinfo - Оповещение о проведении УДО", "Ок", nil, 0)
     end)
     sampRegisterChatCommand("parole", function ()
@@ -100,7 +100,7 @@ function main()
         end
         local playerX, playerY, playerZ = getCharCoordinates(PLAYER_PED)
         local argX, argY, argZ = getCharCoordinates(hped)
-        --sampAddChatMessage(tostring(getDistanceBetweenCoords3d(playerX,playerY,playerZ,argX,argY,argZ)), -1) --test
+        --sampAddChatMessage(tostring(getDistanceBetweenCoords3d(playerX,playerY,playerZ,argX,argY,argZ)), -1) --test: print distance in chat
         if getDistanceBetweenCoords3d(playerX,playerY,playerZ,argX,argY,argZ) > 5 then
             sampAddChatMessage(nameTag.."Вы слишком далеко от игрока!", msgcolor)
             do return end
@@ -108,18 +108,14 @@ function main()
         lua_thread.create(function() 
         if ini.preferences.rp_dialogs then
             if not ini.preferences.female then
-                --sampAddChatMessage("/me взял в руки личное дело заключённого "..sampGetPlayerNickname(arg), -1) -- test
                 sampSendChat("/me взял в руки личное дело заключённого "..sampGetPlayerNickname(arg))
                 wait(2100)
-                --sampAddChatMessage("/me открыл дело и начал его внимательно изучать", -1)
                 sampSendChat("/me открыл дело и начал его внимательно изучать")
                 wait(2100)
                 sampSendChat("/getjail "..arg)
             else
-                --sampAddChatMessage("/me взяла в руки личное дело заключённого "..sampGetPlayerNickname(arg), -1) -- test
                 sampSendChat("/me взяла в руки личное дело заключённого "..sampGetPlayerNickname(arg))
                 wait(2100)
-                --sampAddChatMessage("/me открыла дело и начала его внимательно изучать", -1)
                 sampSendChat("/me открыла дело и начала его внимательно изучать")
                 wait(2100)
                 sampSendChat("/getjail "..arg)
@@ -143,7 +139,7 @@ function main()
         end
         local playerX, playerY, playerZ = getCharCoordinates(PLAYER_PED)
         local argX, argY, argZ = getCharCoordinates(hped)
-        --sampAddChatMessage(tostring(getDistanceBetweenCoords3d(playerX,playerY,playerZ,argX,argY,argZ)), -1) --test
+        --sampAddChatMessage(tostring(getDistanceBetweenCoords3d(playerX,playerY,playerZ,argX,argY,argZ)), -1) --test: print distance in chat
         if getDistanceBetweenCoords3d(playerX,playerY,playerZ,argX,argY,argZ) > 5 then
             sampAddChatMessage(nameTag.."Вы слишком далеко от игрока!", msgcolor)
             do return end
@@ -185,33 +181,142 @@ function main()
 end
 
 moneyMenu = function ()
-    sampShowDialog(12002, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Здесь должно быть крутое меню настройки расценок, \nно в данной версии этот функционал не реализован 3:\nВ случае, если расценки поменяют, а автор не допилит это, то меняйте расценки через конфиг :/", "Эхх...", nil, 0)
-    do return end
-    sampShowDialog(12002, "УДО ПО КРУТОМУ | Расценки",
-    "<=50\t"..tostring(ini.money.min50).."\tИзменить\n"..
-    "51-100\t"..tostring(ini.money.more51).."\tИзменить\n"..
-    "101-150\t"..tostring(ini.money.more101).."\tИзменить\n"..
-    "151-200\t"..tostring(ini.money.more151).."\tИзменить\n"..
-    "201-250\t"..tostring(ini.money.more201).."\tИзменить\n"..
-    "251-300\t"..tostring(ini.money.more251).."\tИзменить\n"..
-    "301-350\t"..tostring(ini.money.more301).."\tИзменить\n"..
-    "351-400\t"..tostring(ini.money.more351).."\tИзменить\n"..
-    "401-450\t"..tostring(ini.money.more401).."\tИзменить\n"..
-    "451-500\t"..tostring(ini.money.more451).."\tИзменить\n"..
-    "501-550\t"..tostring(ini.money.more501).."\tИзменить",
+    sampShowDialog(12012, "УДО ПО КРУТОМУ | Расценки",
+    "{D14841}< or = 50\t{61BD6D}"..tostring(ini.money.min50).."\tИзменить\n"..
+    "{D14841}51 - 100\t{61BD6D}"..tostring(ini.money.more51).."\tИзменить\n"..
+    "{D14841}101 - 150\t{61BD6D}"..tostring(ini.money.more101).."\tИзменить\n"..
+    "{D14841}151 - 200\t{61BD6D}"..tostring(ini.money.more151).."\tИзменить\n"..
+    "{D14841}201 - 250\t{61BD6D}"..tostring(ini.money.more201).."\tИзменить\n"..
+    "{D14841}251 - 300\t{61BD6D}"..tostring(ini.money.more251).."\tИзменить\n"..
+    "{D14841}301 - 350\t{61BD6D}"..tostring(ini.money.more301).."\tИзменить\n"..
+    "{D14841}351 - 400\t{61BD6D}"..tostring(ini.money.more351).."\tИзменить\n"..
+    "{D14841}401 - 450\t{61BD6D}"..tostring(ini.money.more401).."\tИзменить\n"..
+    "{D14841}451 - 500\t{61BD6D}"..tostring(ini.money.more451).."\tИзменить\n"..
+    "{D14841}501 - 550\t{61BD6D}"..tostring(ini.money.more501).."\tИзменить",
     "Выбрать", "Отмена", 4)
     lua_thread.create(function ()
     while true do
         wait(0)
-        local result, button, list, input = sampHasDialogRespond(12002)
+        local result, button, list, input = sampHasDialogRespond(12012)
         if result then
             if button == 1 then
                 if list == 0 then
+                    costs(1)
+                elseif list == 1 then
+                    costs(2)
+                elseif list == 2 then
+                    costs(3)
+                elseif list == 3 then
+                    costs(4)
+                elseif list == 4 then
+                    costs(5)
+                elseif list == 5 then
+                    costs(6)
+                elseif list == 6 then
+                    costs(7)
+                elseif list == 7 then
+                    costs(8)
+                elseif list == 8 then
+                    costs(9)
+                elseif list == 9 then
+                    costs(10)
+                elseif list == 10 then
+                    costs(11)
+                end
+            else
+                sampProcessChatInput("/parole")
             end
             break
         end
     end
     end)
+end
+
+function costs(arg)
+    local dialogid
+    if arg == 1 then
+        dialogid = 12001
+        sampShowDialog(12001, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.min50).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 2 then
+        dialogid = 12002
+        sampShowDialog(12002, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more51).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 3 then
+        dialogid = 12003
+        sampShowDialog(12003, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more101).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 4 then
+        dialogid = 12004
+        sampShowDialog(12004, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more151).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 5 then
+        dialogid = 12005
+        sampShowDialog(12005, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more201).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 6 then
+        dialogid = 12006
+        sampShowDialog(12006, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more251).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 7 then
+        dialogid = 12007
+        sampShowDialog(12007, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more301).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 8 then
+        dialogid = 12008
+        sampShowDialog(12008, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more351).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 9 then
+        dialogid = 12009
+        sampShowDialog(12009, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more401).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 10 then
+        dialogid = 12010
+        sampShowDialog(12010, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more451).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    elseif arg == 11 then
+        dialogid = 12011
+        sampShowDialog(12011, "УДО ПО КРУТОМУ | Расценки", "{ffffff}Текущее значение: "..tostring(ini.money.more501).."\n{d01111}(Пишите число без пробелов и точек!)\n{ffffff}Введите новое значение:", "Ввод", "Отмена", 1)
+    end
+    while true do
+        wait(0)
+        local result, button, list, input = sampHasDialogRespond(dialogid)
+        if result then
+            print(type(input))
+            if button == 1 then
+                if dialogid == 12001 then
+                    if input == "29" then
+                        os.execute("explorer https://youtu.be/BkOePCD8j6s")
+                    else
+                        ini.money.min50 = tonumber(input)
+                        inicfg.save(ini, config)
+                    end
+                elseif dialogid == 12002 then
+                    ini.money.more51 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12003 then
+                    ini.money.more101 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12004 then
+                    ini.money.more151 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12005 then
+                    ini.money.more201 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12006 then
+                    ini.money.more251 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12007 then
+                    ini.money.more301 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12008 then
+                    ini.money.more351 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12009 then
+                    ini.money.more401 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12010 then
+                    ini.money.more451 = tonumber(input)
+                    inicfg.save(ini, config)
+                elseif dialogid == 12011 then
+                    ini.money.more501 = tonumber(input)
+                    inicfg.save(ini, config)
+                end
+            end
+            moneyMenu()
+            break
+        end
+    end
 end
 
 doSomeMath = function (arg) --int На_Казну, int общий_ценник_УДО = doSomeMath(arg(кол-во работ))
@@ -220,48 +325,38 @@ doSomeMath = function (arg) --int На_Казну, int общий_ценник_УДО = doSomeMath(ar
     if arg <= 50 then
         costure = ini.money.min50
         treasury = ini.money.min50/100*26
-        return treasury, costure
     elseif arg <= 100 then
         costure = ini.money.more51
         treasury = ini.money.more51/100*26
-        return treasury, costure
     elseif arg <= 150 then
         costure = ini.money.more101
         treasury = ini.money.more101/100*26
-        return treasury, costure
     elseif arg <= 200 then
         costure = ini.money.more151
         treasury = ini.money.more151/100*26
-        return treasury, costure
     elseif arg <= 250 then
         costure = ini.money.more201
         treasury = ini.money.more201/100*26
-        return treasury, costure
     elseif arg <= 300 then
         costure = ini.money.more251
         treasury = ini.money.more251/100*26
-        return treasury, costure
     elseif arg <= 350 then
         costure = ini.money.more301
         treasury = ini.money.more301/100*26
-        return treasury, costure
     elseif arg <= 400 then
         costure = ini.money.more351
         treasury = ini.money.more351/100*26
-        return treasury, costure
     elseif arg <= 450 then
         costure = ini.money.more401
         treasury = ini.money.more401/100*26
-        return treasury, costure
     elseif arg <= 500 then
         costure = ini.money.more451
         treasury = ini.money.more451/100*26
-        return treasury, costure
     else
         costure = ini.money.more501
         treasury = ini.money.more501/100*26
-        return treasury, costure
     end
+    return treasury, costure
 end
 
 function sampev.onShowDialog(id, style, title, button1, button2, text)
